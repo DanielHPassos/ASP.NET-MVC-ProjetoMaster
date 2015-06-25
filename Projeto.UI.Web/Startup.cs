bb20f8f70a4ec6b9f79ce144ad489912440c3728
fhantom17 <daniel.hpassos@hotmail.com>
2015-06-25 18:26:47 -0300

    Criação dos projetos

---
+++ Projeto.UI.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Projeto.UI.Web.Startup))]
+namespace Projeto.UI.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
